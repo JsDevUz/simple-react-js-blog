a7da9aa3ada05a50ed7440505872581156009534
shakhislom <46161024+shakhislom@users.noreply.github.com>
2023-09-22 21:50:46 +0500

    Update Router.js

--- src/router/Router.js
+++ src/router/Router.js
@@ -5,10 +5,11 @@ import Missing from '../pages/MissingPage'
 import NewPost from '../pages/NewPostPage'
 import PostPage from '../pages/PostPage'
 import About from '../pages/AboutPage'
-function Routers({ handleDelete, posts, addPost, postTitle, postBody, setPostTitle, setPostBody }) {
+function Routers({  handleEdit, editBody, editTitle, setEditBody, setEditTitle, handleDelete, posts, addPost, postTitle, postBody, setPostTitle, setPostBody }) {
     return (
         <Routes>
             <Route exact path='/' element={<Home posts={posts} />} />
+      <Route exact path='/edit/:id' element={<EditPost posts={posts} editTitle={editTitle} editBody={editBody} setEditTitle={setEditTitle} handleEdit={handleEdit} setEditBody={setEditBody} />} />
             <Route exact path='/post' element={<NewPost postTitle={postTitle} postBody={postBody} setPostTitle={setPostTitle} addPost={addPost} setPostBody={setPostBody} />} />
             <Route exact path='/post/:id' element={<PostPage handleDelete={handleDelete} posts={posts} />} />
             <Route exact path='/about' element={<About />} />
